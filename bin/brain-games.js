bd15e2e2614cd560d4b00f5bdec1e06a8c3352dc
Tokimikichika <ahileses@mail.ru>
2024-03-16 10:38:32 +0300

    Игры разума подготовка 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import greetUser from "../scr/cli.js";
+import greetUser from '../scr/cli.js';
 
-console.log("Welcome to the Brain Games!");
-
+console.log('Welcome to the Brain Games!');
 greetUser();
