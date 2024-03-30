6c9c08856a56a4eebd521d31b50287ab5a0ca638
Tokimikichika <ahileses@mail.ru>
2024-03-30 18:55:26 +0300

    немного поменял логику в игра brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,19 +4,16 @@ import getRandomInRange from '../utils.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num <= 1) return 'no';
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
-    if (num % i === 0) {
-      return 'no';
-    }
+  if (num < 2) return false;
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) return false;
   }
-  return 'yes';
+  return true;
 };
-
 const generateRoundPrimeNumberGame = () => {
-  const num = getRandomInRange(1, 991);
+  const num = getRandomInRange(1, 100);
   const question = `${num}`;
-  const answer = isPrime(num);
+  const answer = isPrime(num) ? 'yes' : 'no';
   return [question, answer];
 };
 
